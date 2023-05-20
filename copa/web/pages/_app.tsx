80ee760193dbaeb95e28c2c0cbfd8e63ddd9e712
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-05-20 20:30:46 -0300

    Adicionando o projeto mobile

--- copa/web/pages/_app.tsx
+++ copa/web/pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- copa/web/pages/index.tsx
+++ copa/web/pages/index.tsx
@@ -1,5 +1,17 @@
+import React from "react";
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <h1>Hello NLW</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  console.log(data);
+  return {
+    props: {}
+  }
 }
