c7eacf7edce4234923b9ac074a2afa927f5596f1
MayconInforGames2017 <maycon_gus2009@hotmail.com>
2023-05-21 15:58:04 -0300

    Fonts personalizadas, StatusBar, Customização de temas e criando componentes

--- copa/mobile/App.tsx
+++ copa/mobile/App.tsx
@@ -1,24 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { SignIn } from './src/screens/Signin';
+import { Loading } from './src/components/Loading';
+
+import { THEME } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>NLW Copa do Mundo!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+
+        { fontsLoaded ?  <SignIn /> : <Loading /> }
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 24
-  }
-});
